a3f5918a8059e019f1c6fd2f31f93420dadaabfa
Fardin-E <fardinrahman647@gmail.com>
2024-11-26 15:25:39 -0500

    some ai bs

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 
 func main() {
 	// Serve files from the ./static directory
-	fs := http.FileServer(http.Dir("./static/index.html"))
+	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
+	// Start the server on port 8080
 	log.Println("Starting server on :8080")
-	err := http.ListenAndServe("0.0.0.0:", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
