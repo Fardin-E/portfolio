279c3f2ab570ad7af37daf4f085512807a6fe864
Fardin-E <fardinrahman647@gmail.com>
2024-11-26 14:19:42 -0500

    listening on 8080

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	// Serve files from the ./static directory
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
